d3ce34e7ae27697a59d8ecbc55734980f230db48
branlewalk <branlewalk@gmail.com>
2021-04-10 14:07:30 -0700

    * BrewingContext.cs:
    * Index.cshtml: Updated DbContext to reflect new models. Fixed
      references in Index.cshtml.

--- shire-project/Models/BrewingContext.cs
+++ shire-project/Models/BrewingContext.cs
@@ -5,7 +5,30 @@ namespace shire_project
 {
     public class BrewingContext : DbContext
     {
-        public DbSet<Recipes> Recipe { get; set; }
+        //The Main Tables
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<BrewSession> Sessions { get; set; }
+
+        //Recipe Related
+        public DbSet<MaltIngredient> MaltIngredients { get; set; }
+        public DbSet<HopIngredient> HopIngredients { get; set; }
+        public DbSet<YeastIngredient> YeastIngredients { get; set; }
+        public DbSet<OtherIngredient> OtherIngredients { get; set; }
+        public DbSet<Malt> Malts { get; set; }
+        public DbSet<Hop> Hops { get; set; }
+        public DbSet<Yeast> Yeasts { get; set; }
+        public DbSet<Other> Others { get; set; }
+        public DbSet<Step> Steps { get; set; }
+        public DbSet<Style> Styles { get; set; }
+        public DbSet<BrewImage> BrewImages { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Step> RecipeSteps { get; set; }
+
+        //Session Related
+        public DbSet<Temp> Temps { get; set; }
+        public DbSet<BrewSessionStep> BrewSessionSteps { get; set; }
+
+        //User Tables--TBD
 
         public BrewingContext(DbContextOptions<BrewingContext>options) : base(options)
         {
